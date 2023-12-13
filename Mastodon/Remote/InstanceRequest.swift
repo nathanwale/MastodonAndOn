b5fb472c058c8ca1975a19c27181e015afb732ea
Nathan Wale <nathan.wale@icloud.com>
2023-12-14 10:04:39 +1100

    A remote request to fetch MastodonInstances

---
+++ Mastodon/Remote/InstanceRequest.swift
@@ -0,0 +1,18 @@
+//
+//  InstanceRequest.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 7/12/2023.
+//
+
+import Foundation
+
+struct InstanceRequest: ApiRequest
+{
+    typealias Response = MastodonInstance
+    
+    var host: String
+    let endpoint = "instance"
+    let version = "v2"
+    
+}
