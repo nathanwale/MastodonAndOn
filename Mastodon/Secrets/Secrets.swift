d9c4da9e645ec050d86cf59e1e1f590ff069dc57
Nathan Wale <nathan.wale@icloud.com>
2024-01-15 11:09:11 +1100

    Secrets (with placeholder values)
    This file should not be committed unless you're sure there are no actual secrets in it.
    
    Use `git update-index --assume-unchanged Mastodon/Secrets/Secrets.swift` to avoid accidentally committing secrets.
    
    I expect to see "Avoid Committing Secrets" on a bad English sign somewhere

---
+++ Mastodon/Secrets/Secrets.swift
@@ -0,0 +1,16 @@
+// !!! DO NOT COMMIT CHANGES TO THIS FILE
+// !!! Unless secrets are first scrubbed from it
+
+//  Secrets.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 15/1/2024.
+//
+
+import Foundation
+
+struct Secrets
+{
+    /// Access token for use in previews. DO NOT COMMIT REAL VALUES
+    let previewAccessToken: AccessToken = "REPLACE-ME"
+}
