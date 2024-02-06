b180fad9dca047a9f3fb916ae14952e170f4ba6a
Nathan Wale <nathan.wale@icloud.com>
2024-02-06 15:49:24 +1100

    Animated placeholder animation
    Plus more previews to test

--- Mastodon/Views/Common/WebImage.swift
+++ Mastodon/Views/Common/WebImage.swift
@@ -12,12 +12,25 @@ struct WebImage: View
 {
     var url: URL?
     
+    let placeHolderGradient = LinearGradient(
+        colors: [.purple, .green],
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing)
+    
+    @State private var gradientAngle = 45.0
+    
     var body: some View
     {
         CachedAsyncImage(url: url) {
             $0.resizable()
         } placeholder: {
-            Color.purple.opacity(0.1)
+            placeHolderGradient
+                .hueRotation(.degrees(gradientAngle))
+                .onAppear {
+                    withAnimation(.linear(duration: 1.0).repeatForever(autoreverses: true)) {
+                        gradientAngle = 225.0
+                    }
+                }
         }
         .scaledToFit()
     }
@@ -29,18 +42,29 @@ struct WebImage_Previews: PreviewProvider {
         let cachedUrl = URL(string: "https://files.mastodon.social/accounts/avatars/110/528/637/375/951/012/original/2d14c64b7a9e1f10.jpeg")!
         let freshUrl = URL(string: "https://picsum.photos/1000")!
         let dannyDevito = URL(string: "https://i.imgur.com/6gfUHST.jpeg")
-        HStack {
-            VStack {
-                WebImage(url: cachedUrl)
-                Text("Cached")
+        VStack
+        {
+            HStack 
+            {
+                VStack {
+                    WebImage(url: cachedUrl)
+                    Text("Cached")
+                }
+                VStack {
+                    WebImage(url: freshUrl)
+                    Text("Fresh")
+                }
             }
-            VStack {
-                WebImage(url: freshUrl)
-                Text("Fresh")
-            }
-            VStack {
-                WebImage(url: dannyDevito)
-                Text("Fresh")
+            HStack
+            {
+                VStack {
+                    WebImage(url: dannyDevito)
+                    Text("Danny")
+                }
+                VStack {
+                    WebImage(url: URL(string: "https://example.com/notfound.jpeg")!)
+                    Text("Not Found")
+                }
             }
         }
     }
