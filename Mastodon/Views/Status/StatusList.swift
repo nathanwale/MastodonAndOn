66ecb4d521ec16d56f2a03504173ba236b955f47
Nathan Wale <nathan.wale@icloud.com>
2024-03-12 17:27:31 +1100

    +StatusListRequestView, loads StatusList after Statuses are fetched
    Takes any MastodonStatusRequest, tries to fetch statuses from it
    Displays an indicator while waiting for result
    Displays an error if needed
    StatusList now only takes a loaded [MastodonStatus] as input

--- Mastodon/Views/Status/StatusList.swift
+++ Mastodon/Views/Status/StatusList.swift
@@ -8,12 +8,78 @@
 import SwiftUI
 
 ///
+/// A view that takes a Request object to fetch and display a list of Statuses
+///  - request: A request object to fetch statuses from a server
+///
+struct StatusListRequestView: View
+{
+    ///  A request object to fetch statuses from a server
+    var request: any MastodonStatusRequest
+    
+    /// An error, if one occurs
+    @State var error: Error?
+    
+    /// The list of statuses to display, once fetched
+    @State var statuses: [MastodonStatus]?
+    
+    /// Fetch Statuses from request
+    func fetchStatuses() async
+    {
+        do {
+            statuses = try await request.send()
+        } catch {
+            print(error.localizedDescription)
+            self.error = error
+        }
+    }
+    
+    /// Main view
+    var body: some View
+    {
+        if let statuses {
+            // Statuses have been fetched, show them
+            StatusList(statuses: statuses)
+        } else if let error {
+            // Fetching statuses has produced an error, display
+            errorMessage(error)
+        } else {
+            // By default, show activity indicator, and fetch statuses
+            activityIndicator
+                .task {
+                    await fetchStatuses()
+                }
+        }
+    }
+    
+    /// Activity indicator
+    var activityIndicator: some View
+    {
+        VStack
+        {
+            Text("Fetching Statuses...")
+            ProgressView()
+        }
+    }
+    
+    /// Error message
+    func errorMessage(_ error: Error) -> some View
+    {
+        VStack
+        {
+            Text("Couldn't fetch statuses")
+                .font(.title)
+            Text(error.localizedDescription)
+        }
+    }
+}
+
+///
 /// The default list of statuses, navigable to view replies, etc.
 ///
 struct StatusList: View
 {
     /// Statuses to display
-    @StateObject var source: StatusSource
+    let statuses: [MastodonStatus]
     
     /// App Navigation
     @EnvironmentObject private var navigation: AppNavigation
@@ -32,7 +98,7 @@ struct StatusList: View
     /// Body
     var body: some View
     {
-        List(source.statuses)
+        List(statuses)
         {
             status in
             
@@ -40,120 +106,68 @@ struct StatusList: View
             StatusPost(status)
                 .listRowSeparator(.hidden)
                 .listRowInsets(statusInsets)
-                // colour and event to update focused status
-                .background(background(status: status))
         }
         // apply `scrollable`
         .scrollDisabled(!scrollable)
         // List styling
         .listStyle(.plain)
         .buttonStyle(.borderless)
-        // Pass on navigation object
-        .environmentObject(navigation)
-        // Load source of Statuses
-        .task {
-            await loadStatusesIfRequired()
-        }
-    }
-    
-    /// background colour and event to update focused status
-    func background(status: MastodonStatus) -> some View
-    {
-        // need to read the geometry to tell which post
-        // is in the middle of the screen
-        GeometryReader
-        {
-            geo in
-            
-            // background colour for focused status
-            let bgColour = (status == source.focusedStatus)
-                ? Color.teal.opacity(0.1)
-                : Color.clear
-            
-            // assign colour and set up event
-            Color(bgColour)
-                .onChange(of: geo.frame(in: .global).midY)
-            {
-                let midY = geo.frame(in: .global).midY
-                let screenHeight = UIScreen.main.bounds.height
-                let screenMidY = screenHeight / 2
-                let threshold: CGFloat = screenHeight / 10
-                let isInMiddle = (midY > screenMidY - threshold)
-                                    && (midY < screenMidY + threshold)
-                if isInMiddle {
-                    source.focusedStatus = status
-                    Task {
-                        await loadStatusesIfRequired()
-                    }
-                }
-            }
-        }
-    }
-    
-    /// Load Statuses if required
-    func loadStatusesIfRequired() async
-    {
-        do {
-            try await source.balance()
-        } catch {
-            print(error)
-        }
     }
 }
 
 
 // MARK: - previews
 
-struct Preview_StatusList: View
-{
-    var statuses: [MastodonStatus] = []
-    var request: any MastodonStatusRequest = MockRequestApi()
-    
-    var source: StatusSource
-    {
-        .init(statuses: statuses, request: request)
-    }
-    
-    var body: some View
-    {
-        StatusList(source: source)
-            .environmentObject(AppNavigation())
-    }
-}
+//struct Preview_StatusList: View
+//{
+//    var statuses: [MastodonStatus] = []
+//    var request: any MastodonStatusRequest = MockRequestApi()
+//    
+//    var source: StatusSource
+//    {
+//        .init(statuses: statuses, request: request)
+//    }
+//    
+//    var body: some View
+//    {
+//        StatusList(source: source)
+//            .environmentObject(AppNavigation())
+//    }
+//}
 
 #Preview("Sample posts") 
 {
-    Preview_StatusList(statuses: MastodonStatus.previews)
+    StatusList(statuses: MastodonStatus.previews)
 }
 
 #Preview("Online user posts")
 {
-    Preview_StatusList(request: UserTimelineRequest.sample)
+    StatusListRequestView(request: UserTimelineRequest.sample)
 }
 
 #Preview("Online public timeline")
 {
-    Preview_StatusList(request: PublicTimelineRequest.sample)
+    StatusListRequestView(request: PublicTimelineRequest.sample)
 }
 
 #Preview("Home feed, authorised")
 {
-    Preview_StatusList(request: HomeTimelineRequest.sample)
+    StatusListRequestView(request: HomeTimelineRequest.sample)
 }
 
 #Preview("Home feed, unauthorised")
 {
-    Preview_StatusList(request: HomeTimelineRequest.sampleUnauthorised)
+    StatusListRequestView(request: HomeTimelineRequest.sampleUnauthorised)
 }
 
 #Preview("Isolated post") 
 {
     let statusId = "110879987501995566"
     let statuses = MastodonStatus.previews.filter { $0.id == statusId}
-    return Preview_StatusList(statuses: statuses)
+    return StatusList(statuses: statuses)
 }
 
 #Preview("Hashtag timeline")
 {
-    Preview_StatusList(request: HashtagTimelineRequest.sample)
+    StatusListRequestView(request: HashtagTimelineRequest.sample)
 }
