f186dc071b202e108db4e18887699734fc9252c4
Nathan Wale <nathan.wale@icloud.com>
2024-03-15 10:39:11 +1100

    Request to favourite a Status, given an ID and a host
    Optional `undo` boolean will unfavourite the status if set to true

---
+++ Mastodon/Remote/FavouriteStatusRequest.swift
@@ -0,0 +1,45 @@
+//
+//  FavouriteStatusRequest.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 13/3/2024.
+//
+
+import Foundation
+
+///
+/// Favourite a Status by ID
+///
+struct FavouriteStatusRequest: ApiRequest
+{
+    /// Returns a MastodonStatus
+    typealias Response = MastodonStatus
+    
+    /// Host to send request to
+    let host: String
+    
+    /// ID of Status
+    let statusId: MastodonStatus.Identifier
+    
+    /// Method must be Post
+    let method: HttpMethod = .post
+    
+    /// Endpoint
+    let endpoint: Endpoint
+    
+    /// Access token that authorises action
+    let accessToken: AccessToken?
+    
+    /// Init with host, endpoint, accessToken and optional undo
+    init(host: String, statusId: MastodonStatus.Identifier, undo: Bool = false, accessToken: AccessToken)
+    {
+        self.accessToken = accessToken
+        self.host = host
+        self.statusId = statusId
+        if undo {
+            self.endpoint = .unfavouriteStatus(id: statusId)
+        } else {
+            self.endpoint = .favouriteStatus(id: statusId)
+        }
+    }
+}
