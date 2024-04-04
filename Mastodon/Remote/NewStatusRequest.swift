87ef8043886e680882977134e5d3acf0398c5c4f
Nathan Wale <nathan.wale@icloud.com>
2024-04-05 10:01:00 +1100

    Initial commit: NewStatusRequest
    Posts a new Status if successful

---
+++ Mastodon/Remote/NewStatusRequest.swift
@@ -0,0 +1,66 @@
+//
+//  NewStatusRequest.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 2/4/2024.
+//
+
+import Foundation
+
+struct NewStatusRequest: ApiRequest
+{
+    /// Data type of Post
+    struct NewStatusData: Codable
+    {
+        let status: String
+        let mediaIds: [String]
+        let inReplyToId: MastodonStatus.Identifier?
+        let sensitive: Bool
+        let spoilerText: String
+    }
+    
+    /// Returns a MastodonStatus
+    typealias Response = MastodonStatus
+    
+    /// Host to send request to
+    let host: String
+    
+    /// Method must be Post
+    let method: HttpMethod = .post
+    
+    /// Endpoint
+    let endpoint: Endpoint = .postNewStatus
+    
+    /// Access token that authorises action
+    let accessToken: AccessToken?
+    
+    /// Content of Status
+    let statusContent: String
+    
+    /// ID of Status being replied to. Nil if not a reply
+    var replyStatusId: MastodonStatus.Identifier? = nil
+    
+    /// Should this Status be marked sensitive?
+    var isSensitive = false
+    
+    /// Text to show if Status is sensitive
+    var spoilerText = ""
+    
+    /// Posting requires and idempotency key
+    let requiresIdempotencyKey = true
+    
+    /// Post data
+    var postData: Data?
+    {
+        let object = NewStatusData(
+            status: statusContent, mediaIds: [], inReplyToId: replyStatusId,
+            sensitive: isSensitive, spoilerText: spoilerText)
+        
+        do {
+            return try JsonLoader.encoder.encode(object)
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+}
