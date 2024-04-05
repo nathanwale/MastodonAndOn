620fadf2012a380e9d61c5ecec6fddea4b2fba37
Nathan Wale <nathan.wale@icloud.com>
2024-04-05 16:28:35 +1100

    Initial commit: config object for holding persistent values
    Includes Preview config for using preview values

---
+++ Mastodon/Local/Config.swift
@@ -0,0 +1,72 @@
+//
+//  Persistence.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 5/4/2024.
+//
+
+import SwiftUI
+
+protocol ConfigProvider
+{
+    var activeInstance: String { get set }
+    var accessToken: AccessToken { get }
+}
+
+struct Config: ConfigProvider
+{
+    /// String keys
+    enum Keys: String
+    {
+        case activeInstance = "active-instance"
+        case accessToken = "access-token"
+    }
+    
+    /// Shared instance
+    static let shared: any ConfigProvider = {
+        if isPreviewMode {
+            return PreviewConfig()
+        } else {
+            return Self()
+        }
+    }()
+    
+    /// Are we running in preview mode?
+    static var isPreviewMode: Bool
+    {
+        if let flag = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] {
+            return flag == "1"
+        } else {
+            return false
+        }
+    }
+    
+    /// Active Instance: the instance the user is logged in to
+    @AppStorage(Keys.activeInstance.rawValue)
+    var activeInstance: String = MastodonInstance.defaultHost
+    
+    /// Access token, used for authenticated operations
+    var accessToken: AccessToken
+    {
+        get {
+            if let token = try? KeychainToken.accessToken.retrieve() {
+                return token
+            } else {
+                return "<<TOKEN NOT FOUND>>"
+            }
+        }
+    }
+}
+
+
+///
+/// Configuration for Previews
+///
+struct PreviewConfig: ConfigProvider
+{
+    // Use default instance
+    var activeInstance = MastodonInstance.defaultHost
+    
+    // Use token stored in secrets
+    var accessToken = Secrets.previewAccessToken
+}
