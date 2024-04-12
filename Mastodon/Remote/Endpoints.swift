730da553a0bc43a3dee1f348d33f7d29f059d266
Nathan Wale <nathan.wale@icloud.com>
2024-04-12 10:18:36 +1000

    New endpoint for verifying accounts

--- Mastodon/Remote/Endpoints.swift
+++ Mastodon/Remote/Endpoints.swift
@@ -28,6 +28,7 @@ enum Endpoint
     case mentionLookup
     case postNewStatus
     case editStatus(id: MastodonStatus.Identifier)
+    case verifyAccount
     
     /// Endpoint as path
     var asPath: String
@@ -53,6 +54,7 @@ enum Endpoint
             case .mentionLookup: "/api/v1/accounts/search"
             case .postNewStatus: "/api/v1/statuses"
             case .editStatus(id: let id): "/api/v1/statuses/\(id)"
+            case .verifyAccount: "/api/v1/accounts/verify_credentials"
         }
     }
 }
