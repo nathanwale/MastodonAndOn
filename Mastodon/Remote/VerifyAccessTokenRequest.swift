6188ecad455f47878736af99ce9e6c7b1e33c181
Nathan Wale <nathan.wale@icloud.com>
2024-04-12 10:30:50 +1000

    Renamed file to VerifyAccessTokenRequest
    It's more specifically verifying the Access Token, not the account

---
+++ Mastodon/Remote/VerifyAccessTokenRequest.swift
@@ -0,0 +1,25 @@
+//
+//  AccountRequest.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 16/1/2024.
+//
+
+import Foundation
+
+///
+/// Verify an Access Token, returns a MastodonAccount
+///
+struct VerifyAccessTokenRequest: ApiRequest
+{
+    typealias Response = MastodonAccount
+    
+    /// Host of Mastodon Instance
+    let host: String
+    
+    /// API endpoint
+    let endpoint: Endpoint = .verifyAccessToken
+    
+    /// Access token that authorises action
+    let accessToken: AccessToken?
+}
