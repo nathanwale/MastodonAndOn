7005f5cfd2d9ce034b08b0945a7d898d6770a190
Nathan Wale <nathan.wale@icloud.com>
2024-04-22 15:52:05 +1000

    Pass on login state to content view
    Also some print info

--- Mastodon/MastodonApp.swift
+++ Mastodon/MastodonApp.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 @main
-struct MastodonApp: App {
-    var body: some Scene {
-        WindowGroup {
-//            RootView()
-//                .environmentObject(AppNavigation())
+struct MastodonApp: App 
+{
+    var loginState: ContentView.LoginState
+    {
+        if Config.shared.haveRequiredUserInfo {
+            return .loggedIn
+        } else {
+            return .loggingOut
+        }
+    }
+    
+    var body: some Scene
+    {
+        WindowGroup 
+        {
+            let _ = print("""
+            ...Starting app!
+            Stored user details...
+            Active user id: \(Config.shared.activeAccountIdentifier ?? "None"), 
+            Active instance: \(Config.shared.activeInstanceHost),
+            Access token: \(Config.shared.accessToken)
+            """)
+            
+            ContentView(state: loginState)
         }
     }
 }
