a5dacdab0a227c1deec9d5842989e2db23ea1278
Nathan Wale <nathan.wale@icloud.com>
2024-04-22 15:51:12 +1000

    Handle revoking access, new LoginState enum
    Calls RevokeAccessView
    loginComplete boolean changed to LoginState enum

--- Mastodon/Views/Root/ContentView.swift
+++ Mastodon/Views/Root/ContentView.swift
@@ -9,38 +9,68 @@ import SwiftUI
 
 struct ContentView: View 
 {
+    enum LoginState
+    {
+        case loggedIn
+        case loggingOut
+        case waitingForLogin
+    }
+    
     /// Is login complete
-    @State var loginComplete = false
+    @State var state: LoginState = .waitingForLogin
     
     /// Main view
     var body: some View
     {
-        if loginComplete {
-            rootView
-        } else {
-            loginView
+        switch state {
+            case .loggedIn:
+                rootView
+            case .loggingOut:
+                revokingAccessView
+            case .waitingForLogin:
+                loginView
         }
     }
     
     /// Login view
     var loginView: some View
     {
-        LoginSequenceView() {
+        LoginSequenceView() 
+        {
             print("Log in complete")
-            loginComplete = true
+            state = .loggedIn
         }
     }
     
     /// Root View
     var rootView: some View
     {
-        RootView()
+        RootView() {
+            state = .loggingOut
+        }
+        .environmentObject(AppNavigation())
+    }
+    
+    /// Revoking access view
+    var revokingAccessView: some View
+    {
+        RevokeAccessView(
+            host: Config.shared.activeInstanceHost,
+            accessToken: Config.shared.accessToken)
+        {
+            state = .waitingForLogin
+        }
     }
 }
 
 
 // MARK: - previews
-#Preview
+#Preview("Logged out")
 {
     ContentView()
+}
+
+#Preview("Logged in")
+{
+    ContentView(state: .loggedIn)
 }
